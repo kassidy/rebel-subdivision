5217f0085d24e5df3e6d3df19d889dfe88d0b6f0
Kassidy <kassidy@gmail.com>
2014-01-18 11:06:06 +0000

    Create a new rails app. Edit GemFile to add less-rails, twitter-bootstrap-rails and therubyracer gems. Bundle install. Initial Git commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blackcat::Application.config.secret_key_base = '24c2e621820397a4c960b7d144a92d90b423e57b883d175cf03d11ee5fb73345af81452c57868b28f82477e258dc06489d04603f6454fdfbb35cc20720919173'
